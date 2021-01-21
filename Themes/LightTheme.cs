f02a6f093598fc0e9b306d1880643290e781c7df
Jakub Bartnicki <jakub_bartnicki@tlen.pl>
2021-01-21 13:05:28 +0100

    deleted unnecessary games

---
+++ Themes/LightTheme.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snowman.Themes
+{
+    public class LightTheme : Theme
+    {
+        private LightTheme() { }
+
+        public override Theme getInstance()
+        {
+            if (instance == null)
+            {
+                lock (_lock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new LightTheme();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+}
---
+++ Themes/Theme.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snowman.Themes
+{
+    public abstract class Theme
+    {
+        protected Theme instance;
+        protected static readonly object _lock = new object();
+
+        public abstract Theme getInstance();
+    }
+}
